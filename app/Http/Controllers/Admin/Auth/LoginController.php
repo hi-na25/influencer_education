22c4c2b5fe745c0c62411a90028b074ff9bab6b5
Sakurai <sakurai@genten-group.co.jp>
2026-04-29 22:21:18 +0900

    ログインController修正（エラー対応）

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -40,6 +41,6 @@ class LoginController extends Controller
 
     protected function guard()
     {
-        return \Auth::guard('admin');
+        return Auth::guard('admin');
     }
 }
